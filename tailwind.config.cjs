b8fde92f5fdae5fc7fddd9d30159a267f7fe085c
Singgih Raharjo <sinngihraharjo8@gmail.com>
2026-01-16 12:51:09 +0700

    fix: downgrade tailwindcss to v3 stable to resolve build errors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
         "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
         "./*.{js,ts,jsx,tsx}",
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
-        "./contexts/**/*.{js,ts,jsx,tsx}",
         "./layouts/**/*.{js,ts,jsx,tsx}",
+        "./contexts/**/*.{js,ts,jsx,tsx}"
     ],
     theme: {
         extend: {
